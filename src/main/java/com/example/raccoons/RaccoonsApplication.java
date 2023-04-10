9d523fd81067e80d71b2058ccdb124b365da4d19
Mar_Jimenez <mjimenez@griddynamics.com>
2023-04-10 17:37:41 -0600

    first commit

---
+++ src/main/java/com/example/raccoons/RaccoonsApplication.java
@@ -0,0 +1,13 @@
+package com.example.raccoons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaccoonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaccoonsApplication.class, args);
+	}
+
+}
